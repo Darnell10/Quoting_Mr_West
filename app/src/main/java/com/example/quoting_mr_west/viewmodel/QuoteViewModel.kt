89db590798ebc4653e3cb0b05cfb6a154dad4bcb
Darnell10 <DarnellOtto@ac.c4q.nyc>
2020-04-07 03:57:23 -0400

    create networking call functionality in QuoteViewModel Class

--- app/src/main/java/com/example/quoting_mr_west/viewmodel/QuoteViewModel.kt
+++ app/src/main/java/com/example/quoting_mr_west/viewmodel/QuoteViewModel.kt
@@ -1,12 +1,56 @@
 package com.example.quoting_mr_west.viewmodel
 
 import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.example.quoting_mr_west.models.QuoteApiService
+import com.example.quoting_mr_west.models.Quote_Model
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
 
-class QuoteViewModel(application: Application)  {
+class QuoteViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val quoteApiService = QuoteApiService()
+    private val disposable = CompositeDisposable()
+
+    val quote = MutableLiveData<Quote_Model>()
+    val quoteLoaderError = MutableLiveData<Boolean>()
+    val loading = MutableLiveData<Boolean>()
+
+    fun refresh() {
+
+        fetchFromRemote()
+    }
+
+    private fun fetchFromRemote() {
+        disposable.add(
+            quoteApiService.getQuote()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object : DisposableSingleObserver<Quote_Model>() {
+                    override fun onSuccess(quoteModel: Quote_Model) {
+                        quoteLoaderError.value = false
+                        quote.value = quoteModel
+                        loading.value = false
+                    }
+
+                    override fun onError(e: Throwable) {
+                        e.printStackTrace()
+                        loading.value = false
+                        quote.value = null
+                        quoteLoaderError.value = true
+                    }
+
+                })
+        )
+
+    }
 
 
-
-
-
-
+    override fun onCleared() {
+        super.onCleared()
+        disposable.clear()
+    }
 }
